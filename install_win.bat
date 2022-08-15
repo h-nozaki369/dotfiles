d39d64290406f7087ba9cfba4fce95b46124451b
Hajime Nozaki <nozaki369@outlook.jp>
2022-08-15 23:31:22 +0900

    Modify install_win.bat. %LOCALAPPDATA% seems not working in BAT file.

--- install_win.bat
+++ install_win.bat
@@ -1,4 +1,4 @@
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\vimrc"
-mklink %LOACALAPPDATA%"\init.vim" %HOMEPATH%"\dotfiles\vimrc"
-mklink %LOACALAPPDATA%"\coc-settings.json" %HOMEPATH%"\dotfiles\coc-settings.json"
+mklink "%HOMEPATH%\.vimrc" "%HOMEPATH%\dotfiles\vimrc"
+mklink "C:\Users\%USERNAME%\AppData\Local\nvim\init.vim" "%HOMEPATH%\dotfiles\vimrc"
+mklink "C:\Users\%USERNAME%\AppData\Local\nvim\coc-settings.json" "%HOMEPATH%\dotfiles\coc-settings.json"
 
