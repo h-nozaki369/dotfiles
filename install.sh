6af4f05cc856b3baa7f5cd6ad4bf02d40611368e
Hajime Nozaki <nozaki369@outlook.jp>
2022-08-17 17:47:10 +0900

    Add commands doing mkdir when ~/.config and/or ~/.config/nvim not exist.

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/sh
 ln -sf ~/dotfiles/vimrc ~/.vimrc
+if [ ! -d ~/.config ]; then
+    mkdir ~/.config
+fi
+if [ ! -d ~/.config/nvim ]; then
+    mkdir ~/.config/nvim
+fi
 ln -sf ~/dotfiles/vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/coc-settings.json ~/.config/nvim/coc-settings.json
