878099cf8848841ad58d5886c14c3e25da9e876e
Hajime Nozaki <nozaki369@outlook.jp>
2023-10-02 23:53:07 +0900

    change character of blankline

--- init.lua
+++ init.lua
@@ -140,7 +140,7 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
+      char = '|',
       show_trailing_blankline_indent = false,
     },
   },
